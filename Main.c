701feac0ad2f1673d18bc4f66042519db445ec85
raynan222 <38507894+raynan222@users.noreply.github.com>
2018-06-16 21:38:18 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+		int cmpfunc (const void * a, const void * b) {
+   		return ( *(int*)a - *(int*)b );
+		}
 
 		int main () {
-		char lixo[200],ch,opcao[2],nomearquivo[200],gaussianon[11]={'G','a','u','s','s','i','a','n','o',' '},p2[3]={'P','2'},tmask[2],sobeln[7]={'S','o','b','e','l',' '},laplacen[12]={'L','a','p','l','a','c','i','a','n','o',' '},prewittn[9]={'P','r','e','w','i','t','t',' '};
+		char lixo[200],ch,opcao[2],nomearquivo[200],gaussianon[11]={'G','a','u','s','s','i','a','n','o',' '},p2[3]={'P','2'},tmask[2],sobeln[7]={'S','o','b','e','l',' '},laplacen[12]={'L','a','p','l','a','c','i','a','n','o',' '},prewittn[9]={'P','r','e','w','i','t','t',' '},Denoise[9]={'D','e','n','o','i','s','e',' '};
 		unsigned char **matriz,aux;
 		float **matrizvariavel,**matrizrec,**matrizexp,soma,mask=3.0;
-		int alt=-1,lar=-1,count=0,esc=-1,j,i,k,l,m,n,a,b,c,zero=0;
+		int alt=-1,lar=-1,count=0,esc=-1,j,i,k,l,m,n,a,b,c,zero=0,*vet,maskint;
 		int laplace[3][3] = {{0, -1, 0},{-1, 4, -1},{0, -1, 0}};
 		int prewitt[3][3] = {{-1, 0, 1},{-1, 0, 1},{-1, 0, 1}};
 		int sobel[3][3] = {{-1, 0, 1},{-2, 0, 2},{-1, 0, 1}};
@@ -30,7 +33,7 @@
 
 		while (count==0){
 			system("clear");
-			printf("\nArquivo selecionado: %s\n\nSelecione uma das opções abaixo:\n\n(A)Aplicar filtro de Sobel.\n(B)Aplicar filtro Laplaciano.\n(C)Aplicar filtro de prewitt.\n(D)Aplicar filtro Gaussiano(3x3).\n(E)Aplicar filtro Gaussiano(5x5).\n(F)Aplicar filtro Gaussiano(7x7).\n(G)Aplicar filtro da média ponderada.\n(X)Sair.\n\n",nomearquivo);
+			printf("\nArquivo selecionado: %s\n\nSelecione uma das opções abaixo:\n\n(A)Aplicar filtro de Sobel.\n(B)Aplicar filtro Laplaciano.\n(C)Aplicar filtro de prewitt.\n(D)Aplicar filtro Gaussiano(3x3).\n(E)Aplicar filtro Gaussiano(5x5).\n(F)Aplicar filtro Gaussiano(7x7).\n(G)Aplicar filtro denoise(3x3).\n(H)Aplicar filtro denoise(5x5).\n(I)Aplicar filtro denoise(9x9).\n(X)Sair.\n\n",nomearquivo);
 			gets(&opcao);
 			if(opcao[zero]=='a'||opcao[zero]=='A'||opcao[zero]=='1'){
 				count=10;
@@ -53,7 +56,19 @@
 				count=40;
 				mask=7.0;
 			}
-			if(opcao[zero]=='x'||opcao[zero]=='X'||opcao[zero]=='9'){
+			if(opcao[zero]=='g'||opcao[zero]=='G'||opcao[zero]=='7'){
+				count=50;
+				mask=3.0;
+			}
+			if(opcao[zero]=='h'||opcao[zero]=='H'||opcao[zero]=='8'){
+				count=50;
+				mask=5.0;
+			}
+			if(opcao[zero]=='i'||opcao[zero]=='I'||opcao[zero]=='9'){
+				count=50;
+				mask=7.0;
+			}
+			if(opcao[zero]=='x'||opcao[zero]=='X'||opcao[zero]=='0'){
 				system("clear");
 				exit(1);
 			}
@@ -202,6 +217,7 @@
 				strcat(sobeln,nomearquivo);
 				ps = fopen(sobeln, "w");
 				fprintf(ps,"%s\n",p2);
+				fprintf(ps,"#Aplicação de filtro Sobel criada por Raynan S. (IFCE)\n");
 				fprintf(ps,"%d %d\n",lar, alt);
 				fprintf(ps,"%d\n",esc);
 				c=0;
@@ -249,6 +265,7 @@
 				strcat(laplacen,nomearquivo);
 				ps = fopen(laplacen, "w");
 				fprintf(ps,"%s\n",p2);
+				fprintf(ps,"#Aplicação de filtro Laplaciana criada por Raynan S. (IFCE)\n");
 				fprintf(ps,"%d %d\n",lar, alt);
 				fprintf(ps,"%d\n",esc);
 				c=0;
@@ -265,7 +282,7 @@
 			}
 
 			/////////////////
-		  //Filtro Prewit//
+		  //Filtro Prewitt//
 		 /////////////////
 
 			if(count==30){
@@ -296,6 +313,7 @@
 				strcat(prewittn,nomearquivo);
 				ps = fopen(prewittn, "w");
 				fprintf(ps,"%s\n",p2);
+				fprintf(ps,"#Aplicação de filtro Prewitt criada por Raynan S. (IFCE)\n");
 				fprintf(ps,"%d %d\n",lar, alt);
 				fprintf(ps,"%d\n",esc);
 				c=0;
@@ -346,6 +364,7 @@
 				strcat(gaussianon,nomearquivo);
 				ps = fopen(gaussianon, "w");
 				fprintf(ps,"%s\n",p2);
+				fprintf(ps,"#Aplicação de filtro Gaussiana de %.0fx%.0f criada por Raynan S. (IFCE)\n",mask,mask);
 				fprintf(ps,"%d %d\n",lar, alt);
 				fprintf(ps,"%d\n",esc);
 				c=0;
@@ -360,6 +379,56 @@
 					}
 				}
 			}
+
+			 /////////////////////
+			//Filtro de Denoise//
+		  /////////////////////
+
+			if(count==50){
+				vet=(int*)malloc(mask*mask*sizeof(int));
+				for (i=floor(mask/2); i<alt+floor(mask/2); i++) {
+					for (j=floor(mask/2); j<lar+floor(mask/2); j++) {
+						maskint=0;
+						soma=0;				
+						for (k=i-floor(mask/2),a=0;a<mask; k++, a++) {
+							for (l=j-floor(mask/2),b=0;b<mask; l++, b++) {
+								vet[maskint++]=matriz[k][l];
+							}
+						}
+						qsort(vet, mask*mask, sizeof(int), cmpfunc);
+						maskint=floor((mask*mask)/2);
+						matrizrec[i][j]=vet[maskint];
+					}
+				}
+
+				 ///////////////////
+				//Salva o arquivo//
+			  ///////////////////
+
+				sprintf (tmask,"%.0f",mask);
+				strcat(Denoise,tmask);
+				strcat(Denoise,"x");
+				strcat(Denoise,tmask);
+				strcat(Denoise," ");
+				strcat(Denoise,nomearquivo);
+				ps = fopen(Denoise, "w");
+				fprintf(ps,"%s\n",p2);
+				fprintf(ps,"#Aplicação de filtro Denoise de %.0fx%.0f criada por Raynan S. (IFCE)\n",mask,mask);
+				fprintf(ps,"%d %d\n",lar, alt);
+				fprintf(ps,"%d\n",esc);
+				c=0;
+				for(i=0; i<alt; i++){
+					for(j=0; j<lar; j++){
+						fprintf(ps,"%.0f\t\t",matrizrec[i][j]);
+						c++;
+						if (c>=10){
+							fprintf(ps,"\n");
+							c=0;
+						}
+					}
+				}
+			}
+
 
 		system("clear");
 		exit(1);
